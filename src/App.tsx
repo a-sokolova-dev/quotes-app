14a9ffd921795a6631e3854532b2069ba7ec4073
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-09-14 14:49:23 +0300

    chore: setup mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from '@mui/material/styles'
 import type { JSX } from 'react'
+import { THEME } from './theme.ts'
 
 const App = (): JSX.Element => {
-  return <h1>Vite + React</h1>
+    <ThemeProvider theme={THEME}>
+      <h1>Vite + React</h1>
+    </ThemeProvider>
 }
 
 export default App
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material'
+
+export const THEME = createTheme({
+  typography: {
+    fontFamily: `"IBM Plex Mono", "Roboto", "Helvetica", "Arial", sans-serif`
+  }
+})
