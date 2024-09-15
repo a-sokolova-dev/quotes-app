ef0addaaab02c10ed2c4f2c683e9220ce7f31c12
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-09-15 19:23:58 +0300

    refactor: move vitest-setup to src

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/tests/vitest-setup.ts',
+    setupFiles: './src/vitest-setup.ts',
   }
 })
